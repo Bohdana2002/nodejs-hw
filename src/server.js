e7e7356cb9bdb178ac6e9c7fde4bcf5ea3fb235d
Bohdana Turcheniak <bogdanaturcheniak@gmail.com>
2026-07-05 22:36:09 +0300

    "started doing hometask: installed dependencies, stopped on middleware'

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { id_param } = req.params.noteId;
+  res.status(200).json({ message: `Retrieved note with ID: ${id_param}` });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

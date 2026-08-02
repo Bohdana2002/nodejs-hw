89a5465175f0ac4562ebfd1c094ab1e640417b9d
Bohdana Turcheniak <bogdanaturcheniak@gmail.com>
2026-08-02 17:51:42 +0200

    fixed erroHandler.js, added updateNote

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,11 @@
+import { isHttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
+
+  if (isHttpError(err)) {
+    return res.status(err.status).json({ message: err.message });
+  }
 
   const isProd = process.env.NODE_ENV === 'production';
 
